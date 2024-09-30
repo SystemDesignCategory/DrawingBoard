8fae152ac7ade4c26580c9fd3b1133c433a5bdbb
somayeh farahani <s.ziarati@gmail.com>
2024-09-30 17:34:55 +0330

    add drawingBoard Project

---
+++ DrawingBoard/Hubs/ConvasHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DrawingBoard.Hubs
+{
+    public class CanvasHub : Hub
+    {
+        // Method that will be called by the client to broadcast drawing coordinates
+        public async Task DrawOnCanvas(int x1, int y1, int x2, int y2)
+        {
+            // Broadcast the coordinates to all connected clients
+            await Clients.Others.SendAsync("ReceiveDraw", x1, y1, x2, y2);
+        }
+        public async Task ClearCanvas()
+        {
+            await Clients.Others.SendAsync("ClearCanvas");
+        }
+    }
+}
